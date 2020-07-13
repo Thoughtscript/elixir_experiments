8e15333c2a88052d059730ce2d02036f26b14949
Adam In Tae Gerard <adam.gerard@gmail.com>
2020-07-12 17:28:19 -0700

    update

--- elixir_docker/4-use-dockerfile/1-use-dockerfile.sh
+++ elixir_docker/4-use-dockerfile/1-use-dockerfile.sh
@@ -7,7 +7,18 @@ docker login -u USERNAME -p "PASSWORD"
 
 # Build from dockerfile
 ## Use this over `docker build - < Dockerfile`
+## Note that the dockerfile copies in ANY files in this directory
+## Feel free to drop in a whole mix project
 docker build .
+## Get the CONTAINER ID <b671c7aa4ffc> from the end of this
+
+# Connect to the CONTAINER 
+docker run -it --entrypoint bash <b671c7aa4ffc>
+# Or, if it's currently running
+docker exec -it <b671c7aa4ffc> bash
+
+# Execute script
+elixir helloworld.exs
 
 # Cleanup
 docker system prune --volumes
