33e2e8fca806ac16e069132c86f6cda78ffa7564
Adam In Tae Gerard <adam.gerard@gmail.com>
2020-07-12 17:14:59 -0700

    corrected bash

--- elixir_docker/3-create-app-in-volume/1-create-app-in-volume.sh
+++ elixir_docker/3-create-app-in-volume/1-create-app-in-volume.sh
@@ -12,16 +12,13 @@ docker volume create volume_example_2
 # View the volumes
 docker volume ls
 
-# Create the app in the local directory
-## Navigate to the elixir-mix directory in here
-docker container run --rm -v $PWD:/data -w /data elixir mix new mix_example_2
-
 # Run the Docker container using the created volume
 ## Open bash
 docker container run --rm -v volume_example_2:/root/ -v $PWD:/data -w /data -it elixir bash
 
 ## Execute in container Bash:
 mix new example
+cd example
 MIX_ENV=test mix test
 mix run
 ## Note: you may not see any outputs simply because the default mix files don't print
@@ -30,11 +27,11 @@ mix run
 cd lib
 iex
 ### iex(1)> 
-c "./mix_example1.ex"
-### > [MixExample1]
+c "./example.ex"
+### > [Example]
 
 ### iex(2)> 
-MixExample1.hello()
+Example.hello()
 ### > :world
 ## Above, the .ex file was loaded into iex as a dependency then executed
 
